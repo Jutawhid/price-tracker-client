75695b6f311d1ad09a63d1cb48e3ce7fa1693fbd
Jusim Uddin <jutawhid@gmail.com>
2024-12-22 01:07:14 +0600

    next-auth

--- features/auth/next-auth/next-auth.options.ts
+++ features/auth/next-auth/next-auth.options.ts
@@ -16,8 +16,7 @@ export const AuthOptions: NextAuthOptions = {
       // This function is called at server side only
       async authorize(credentials) {
         try {
-          const API_URL = `http://localhost:8080/api/v1/users/login`;
-          console.log("🚀 ~ authorize ~ API_URL:", API_URL);
+          const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/login`;
           const res = await fetch(API_URL, {
             method: "POST",
             body: JSON.stringify({
